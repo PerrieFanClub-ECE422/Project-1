86e03cac130c6fce8037f1600d9df6ae491a9dec
Ubuntu <guerrapa@ualberta.ca>
2024-02-08 23:51:39 +0000

    added wait function to avoid scale pile up

--- autoscaler.py
+++ autoscaler.py
@@ -1,7 +1,6 @@
 import docker
 import requests
 import time
-import sys
 from datetime import datetime
 import matplotlib.pyplot as plt
 
@@ -11,14 +10,13 @@ response_time_data=[]
 time_data_size = []
 time_data_response = []
 
-
 UPPER_RESPONSE_THRESHOLD = 3
 LOWER_RESPONSE_THRESHOLD = 2
 SCALE_IN_FACTOR = 0.5
 SCALE_OUT_FACTOR = 2
 MONITOR_PERIOD = 20
 MAX_CONTAINERS = 128
-MIN_CONTAINERS = 4
+MIN_CONTAINERS = 1
 scaling_enabled = False
 
 # docker client
@@ -26,19 +24,19 @@ try:
     client = docker.DockerClient(base_url='unix://var/run/docker.sock')
 except Exception as exc:
     print("client error = ", exc)
+    exit()
 
 # get swarm metric output
 def get_metrics():
     try:
         metric_response = requests.get(current_ip)
         if metric_response.status_code == 200:
-            # print(metric_response.text)
             return metric_response.text
         else:
             print("status code error")
             return None
     except Exception as e:
-        print("Error:", e)
+        print("Get Metrics Error:", e)
 
 
 # time the get_metrics() func
@@ -50,16 +48,28 @@ def get_metrics_time():
     
 
 def get_current_containers():
-    return len( client.services.get("/app_name_web").tasks() ) - 2
+    app_name_web_service = client.services.get("app_name_web")
+    app_name_web_tasks = app_name_web_service.tasks(filters={"service": app_name_web_service.id})
+    running_web_tasks = [task for task in app_name_web_tasks if task["Status"]["State"] == "running"]
+    return len(running_web_tasks)
 
+def wait_until_container_count(target_count):
+    while True:
+        current_count = get_current_containers()
+        if current_count == target_count:
+            print("Target container count reached:", target_count)
+            break
+        print("Waiting for container count to reach", target_count, "Current count:", current_count)
+        time.sleep(5)
 
 def scale_service(scale_amount):
     try:
         service = client.services.get("app_name_web")
         service.scale(scale_amount)
-        print("******* service scale amount === ", scale_amount)
+        print("*** Service Scale Amount = ", scale_amount, " ***")
+        wait_until_container_count(scale_amount)
     except Exception as err:
-        print("error scaling: ", err)
+        print("Error Scaling: ", err)
 
 
 def update_size_plot():
@@ -71,7 +81,6 @@ def update_size_plot():
     plt.xlabel('Time')
     plt.ylabel('# of Containers')
     plt.savefig("application_size_plot.png")
-
 
 # autoscale func
 def autoscale():
@@ -88,37 +97,30 @@ def autoscale():
             if metric_time:
                 update_size_plot()
                 num_samples += 1
-                print("\tsample #", num_samples)
+                print("\tSample #", num_samples)
                 total_response_time += metric_time
-                print("\tmetric = ", metric_time, "\ttotal = ", total_response_time)
+                print("\tMetric = ", metric_time, "\tTotal = ", total_response_time)
             else:
-                print(">>> ERROR - could not get metric time")
+                print(">>> ERROR - Could not get metric time")
 
-            print("\tcurr containers: ", get_current_containers())
-            print("")
-            time.sleep(1)
-
+            print("\tCurrent Containers: ", get_current_containers())
 
         # check to see if there are any samples
         if num_samples > 0:
-
             average_response_time = total_response_time / num_samples
-            print("******* avgresp time: ", average_response_time)
-            
-            #update_response_plot(average_response_time)
-            if average_response_time > UPPER_RESPONSE_THRESHOLD and scaling_enabled:
-                print("getcurrcon ", get_current_containers(), "    calcscaleout ", SCALE_OUT_FACTOR)
-                scale_service( min(MAX_CONTAINERS, int( get_current_containers() * SCALE_OUT_FACTOR) ) )
-            
-            elif average_response_time < LOWER_RESPONSE_THRESHOLD and scaling_enabled:
-                print("getcurrcon ", get_current_containers(), "    calcscalein ", SCALE_IN_FACTOR)
-                scale_service( max(MIN_CONTAINERS, int( get_current_containers() * SCALE_IN_FACTOR )) )
+            print("*** Average Response time: ", average_response_time, " ***")     
 
-        print("!!!!!!! sleeping")
-        time.sleep(1)
-        print("!!!!!!! done sleeping")
-        print("")
-
+            if scaling_enabled and average_response_time > UPPER_RESPONSE_THRESHOLD:
+                print("*** Average Response Time > Upper Response Threshold= ", UPPER_RESPONSE_THRESHOLD, " ***")
+                print("*** Scaling Enabled therefore Scale out! ***")
+                print("*** Current Containers: ", get_current_containers(), "    Scale out Factor: ", SCALE_OUT_FACTOR, " ***")
+                scale_service(min(MAX_CONTAINERS,get_current_containers() * SCALE_OUT_FACTOR))
+            
+            elif scaling_enabled and average_response_time < LOWER_RESPONSE_THRESHOLD:
+                print("*** Average Response Time < Lower Response Threshold= ", LOWER_RESPONSE_THRESHOLD, " ***")
+                print("*** Scaling Enabled therefore Scale in! ***")
+                print("Current Containers: ", get_current_containers(), "    Scale in Factor: ", SCALE_IN_FACTOR)
+                scale_service(max(MIN_CONTAINERS, int(get_current_containers() * SCALE_IN_FACTOR)))
 
 
 if __name__ == "__main__":
@@ -129,6 +131,8 @@ if __name__ == "__main__":
 
     service = client.services.get("app_name_web")
     service.scale(MIN_CONTAINERS)
+    # if there is existing containers, reset to 1
+    wait_until_container_count(MIN_CONTAINERS)
 
     if userInput.lower() == "y":
         print("SCALING HAS BEEN ENABLED")
